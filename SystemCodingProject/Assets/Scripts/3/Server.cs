ff76b0e237aa9b49b2bb02972bd6f4043e63560c
Георгий Чудаков <george1005@yandex.ru>
2021-12-05 16:04:03 +0300

    3 hm (2/3)
    на 3 нет сил и времени, н понимаю как сделать.

---
+++ SystemCodingProject/Assets/Scripts/3/Server.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace _3
+{
+
+public class Server : MonoBehaviour
+{
+    private const int MAX_CONNECTION = 10;
+
+    private int port = 5805;
+
+    private int hostID;
+    private int reliableChannel;
+
+    private bool isStarted = false;
+    private byte error;
+
+    List<int> connectionIDs = new List<int>();
+    private Dictionary<int, string> idsAndNames = new Dictionary<int, string>();
+    public void StartServer()
+    {        
+        NetworkTransport.Init();
+
+        ConnectionConfig cc = new ConnectionConfig();
+        reliableChannel = cc.AddChannel(QosType.Reliable);
+
+        HostTopology topology = new HostTopology(cc, MAX_CONNECTION);        
+        hostID = NetworkTransport.AddHost(topology, port);
+
+        isStarted = true;
+    }
+
+    void Update()
+    {
+        if (!isStarted)
+            return;
+
+        int recHostId;
+        int connectionId;
+        int channelId;
+        byte[] recBuffer = new byte[1024];
+        int bufferSize = 1024;
+        int dataSize;
+        NetworkEventType recData = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out error);
+
+        while (recData != NetworkEventType.Nothing)
+        {
+            switch (recData)
+            {
+                case NetworkEventType.Nothing:
+                    break;
+
+                case NetworkEventType.ConnectEvent:
+                    connectionIDs.Add(connectionId);
+                    SendMessage($"Set your name,using chat",connectionId);
+                    SendMessageToAll($"Player {connectionId} has connected.");
+                    Debug.Log($"Player {connectionId} has connected.");
+                    break;
+
+                case NetworkEventType.DataEvent:
+                    string message = Encoding.Unicode.GetString(recBuffer, 0, dataSize);
+                    if (!FindNameSet(connectionId))
+                    {
+                        idsAndNames.Add(connectionId,message);
+                        SendMessage($"Your name is {message}", connectionId);
+                        Debug.Log($"Your name is {message}");
+                    }
+                    else
+                    {
+                        SendMessageToAll($"{idsAndNames[connectionId]}: {message}");
+                        Debug.Log($"{idsAndNames[connectionId]}: {message}");
+                    }
+                    
+                    break;
+
+                case NetworkEventType.DisconnectEvent:
+                    idsAndNames.Remove(connectionId);
+                    connectionIDs.Remove(connectionId);
+                    SendMessageToAll($"{idsAndNames[connectionId]} has disconnected.");
+                    Debug.Log($"{idsAndNames[connectionId]} has disconnected.");
+                    break;
+
+                case NetworkEventType.BroadcastEvent:
+                    break;
+
+            }
+
+            recData = NetworkTransport.Receive(out recHostId, out connectionId, out channelId, recBuffer, bufferSize, out dataSize, out error);
+        }
+    }
+
+    public void ShutDownServer()
+    {
+        if (!isStarted)
+            return;
+
+        NetworkTransport.RemoveHost(hostID);
+        NetworkTransport.Shutdown();
+        isStarted = false;
+    }
+
+    private bool FindNameSet(int connectionID)
+    {
+        return idsAndNames.ContainsKey(connectionID);
+    }
+    
+    public void SendMessage(string message, int connectionID)
+    {
+        byte[] buffer = Encoding.Unicode.GetBytes(message);
+        NetworkTransport.Send(hostID, connectionID, reliableChannel, buffer, message.Length * sizeof(char), out error);
+        if ((NetworkError)error != NetworkError.Ok)
+            Debug.Log((NetworkError)error);
+    }
+
+    public void SendMessageToAll(string message)
+    {
+        for (int i = 0; i < connectionIDs.Count; i++)        
+            SendMessage(message, connectionIDs[i]);        
+    }
+}
+
+}
---
+++ SystemCodingProject/Assets/Scripts/SecondHomework.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Jobs;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SecondHomework : MonoBehaviour
+{
+    private NativeArray<int> _array;
+    private NativeArray<Vector3> _positions;
+    private NativeArray<Vector3> _velocities;
+    private NativeArray<Vector3> _finalPositions;
+    private JobHandle _handle;
+   
+
+    private void Start()
+    {
+        #region first_task
+
+        // _array = new NativeArray<int>(10, Allocator.TempJob);
+        // for (int i = 0; i < _array.Length; i++)
+        // {
+        //     _array[i] = Random.Range(0, 15);
+        // }
+        // for (int i = 0; i < _array.Length; i++)
+        // {
+        //     Debug.Log(_array[i]);
+        // }
+        // JobStruct jobStruct = new JobStruct();
+        // jobStruct.Array = _array;
+        // jobStruct.Execute();
+        // for (int i = 0; i < _array.Length; i++)
+        // {
+        //     Debug.Log(_array[i]);
+        // }
+
+        #endregion
+
+        _positions = new NativeArray<Vector3>(100, Allocator.TempJob);
+        for (int i = 0; i < _positions.Length; i++)
+        {
+            _positions[i] = new Vector3(Random.Range(0, 10), Random.Range(0, 10), Random.Range(0, 10));
+        }
+
+        _velocities = new NativeArray<Vector3>(100, Allocator.TempJob);
+        for (int i = 0; i < _velocities.Length; i++)
+        {
+            _velocities[i] = new Vector3(Random.Range(0, 40), Random.Range(0, 40), Random.Range(0, 40));
+        }
+        _finalPositions= new NativeArray<Vector3>(100,Allocator.TempJob);
+        JobStruct job = new JobStruct();
+        job.Positions = _positions;
+        job.Velocities = _velocities;
+        job.FinalPositions = _finalPositions;
+
+        _handle = job.Schedule(100, 10);
+        _handle.Complete();
+        StartCoroutine(JobCoroutine());
+
+    }
+
+    IEnumerator JobCoroutine()
+    {
+        while (_handle.IsCompleted == false)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        foreach (Vector3 vector in _finalPositions)
+        {
+            print(vector);
+        }
+        
+        
+        
+    }
+
+    // public struct JobStruct :  IJob
+    // {
+    //     public NativeArray<int> Array;
+    //
+    //     public void Execute()
+    //     {
+    //         for (int i = 0; i < Array.Length; i++)
+    //         {
+    //             if (Array[i] > 10)
+    //             {
+    //                 Array[i] = 0;
+    //             }
+    //         }
+    //     }
+    // }
+    public struct JobStruct : IJobParallelFor
+    {
+        public NativeArray<Vector3> Positions;
+        public NativeArray<Vector3> Velocities;
+        public NativeArray<Vector3> FinalPositions;
+
+        
+        public void Execute(int index)
+        {
+            FinalPositions[index] = Positions[index] + Velocities[index];
+        }
+
+       
+
+        
+    }
+    private void OnDestroy()
+    {
+        _positions.Dispose();
+        _velocities.Dispose();
+        _finalPositions.Dispose();
+    }
+}
